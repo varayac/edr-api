4673ed70eb10899c93b56b8ba49b1cdc013f4a0e
varayac <victor.d.araya@gmail.com>
2022-04-06 17:26:42 -0400

    lmao

---
+++ database/db.js
@@ -0,0 +1,8 @@
+import { Sequelize } from 'sequelize'
+
+const conn = new Sequelize('edr_db', 'root', 'pass11', {
+  host: '192.168.0.25',
+  dialect: 'mariadb'
+})
+
+export default db
---
+++ models/UsersModel.js
@@ -0,0 +1,4 @@
+// Import connection DB
+import db from '../database/db'
+// Import sequelize
+import { DataTypes } from 'sequelize'
